b36507a9bafab259191bc5a0df3cfec82da5d5e4
luisotaviodesimone <luisotaviodesimone@gmail.com>
2025-05-02 13:52:48 -0300

    feat: create insert queries

---
+++ internal/store/pgstore/queries/queries.sql
@@ -0,0 +1,19 @@
+-- name: InsertPing :one
+INSERT INTO
+  ping_logs (
+    'latency_avg_ms',
+    'loss_rate_percent',
+    'ping_count'
+  )
+VALUES
+  ($1, $2, $3)
+RETURNING
+  'id';
+
+-- name: InsertHttp :one
+INSERT INTO
+  http_logs ('latency_avg_ms', 'http_count')
+VALUES
+  ($1, $2)
+RETURNING
+  'id';
